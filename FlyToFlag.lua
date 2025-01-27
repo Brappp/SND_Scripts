5053e138185bc9805816b3461b96c24329e74890
Wah <74884113+Brappp@users.noreply.github.com>
2025-01-26 21:19:41 -0500

    Update FlyToFlag.lua

--- FlyToFlag.lua
+++ FlyToFlag.lua
@@ -19,7 +19,6 @@ function EnsureMounted()
     local conditionsPreventingMount = {}
 
     if IsCharacterInCondition("mounted") then
-        yield('/echo [MOUNT] Already mounted.')
         UseNavmesh()
         return
     end
@@ -33,46 +32,36 @@ function EnsureMounted()
     end
 
     if #conditionsPreventingMount > 0 then
-        yield('/echo [MOUNT] Cannot mount due to: ' .. table.concat(conditionsPreventingMount, ", "))
         return
     end
 
-    yield('/echo [MOUNT] Attempting to mount using "mount roulette"...')
     yield('/gaction "mount roulette"')
     yield('/wait 4')
 
     if not IsCharacterInCondition("mounted") then
-        yield('/echo [MOUNT] Mounting failed. Retrying...')
         EnsureMounted()
     else
-        yield('/echo [MOUNT] Successfully mounted.')
         UseNavmesh()
     end
 end
 
 -- Activate navmesh and monitor pathfinding status
 function UseNavmesh()
-    yield('/echo [NAVMESH] Activating navmesh for movement...')
     yield('/vnav flyflag')
     yield('/wait 3')
 
     while PathIsRunning() do
-        yield('/echo [NAVMESH] Pathfinding in progress...')
         yield('/wait 3')
     end
 
-    yield('/echo [NAVMESH] Navigation completed.')
     Dismount()
 end
 
 -- Dismount the player if currently mounted
 function Dismount()
     if IsCharacterInCondition("mounted") then
-        yield('/echo [MOUNT] Dismounting...')
         yield('/gaction "dismount"')
         yield('/wait 1')
-    else
-        yield('/echo [MOUNT] Already dismounted.')
     end
 end
 
